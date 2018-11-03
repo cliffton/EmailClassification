c2a20f56691cfaf9977fe7abe0a54dd261f11e1f
cliffton <cliffton.fernandes.rulz@gmail.com>
2018-11-03 15:42:25 -0400

    data

--- python/print_email.py
+++ python/print_email.py
@@ -185,7 +185,7 @@ def separate_attrs2(email_file_path, count):
                 tmp += e
 
 
-    msg = str(count) + ",1, " + tmp.lower() + "\n" 
+    msg = str(count) + ",0, " + tmp.lower() + "\n" 
     email_file.close()
     return msg
 
@@ -194,8 +194,8 @@ def for_files_in_folder2(folder):
     all_files = []
 
     for f in listdir(folder):
-        if isfile(join(folder, f)) and (f[-3:] == "txt" or f[-3:] == "eml") and (f != "Summary.txt") :
-        # if isfile(join(folder, f)):
+        # if isfile(join(folder, f)) and (f[-3:] == "txt" or f[-3:] == "eml") and (f != "Summary.txt") :
+        if isfile(join(folder, f)):
             all_files.append(join(folder, f))
         elif not isfile(join(folder, f)):
             extra_files = for_files_in_folder(join(folder, f))
@@ -206,9 +206,9 @@ def for_files_in_folder2(folder):
 def mt_csv2(folder):
     all_files = for_files_in_folder2(folder)
     count = 0
-    output = open("/home/cliffton/workspace/EmailData/output/spam2.csv", "w+")
+    output = open("/home/cliffton/workspace/EmailData/output/ham3.csv", "w+")
     for x in all_files:
-        if x.find("spam/") > -1:
+        if x.find("ham/") > -1:
             print(x)
             try:
                 # print(str(x))
@@ -225,16 +225,20 @@ def mt_csv2(folder):
 
 
 def mt2():
-    all_files = all_folders_in_folder("/home/cliffton/workspace/EmailData/data/enron/spam")
+    all_files = all_folders_in_folder("/home/cliffton/workspace/EmailData/data/assasin/spam/spam")
     # pool = ThreadPool(8)
     # results = pool.map(mt_csv2, all_files)
     for folder in all_files:
         mt_csv2(folder)
 
 
+def mt3():
+    mt_csv2("/home/cliffton/workspace/EmailData/data/assasin/ham/easy_ham")
+
+
 
 # users()
-mt2()
+mt3()
 
 
 
