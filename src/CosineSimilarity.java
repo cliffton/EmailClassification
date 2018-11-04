38efb120033b8e45fd40f6eef4ccd5b8aea79795
cliffton <cliffton.fernandes.rulz@gmail.com>
2018-11-04 14:14:13 -0500

    cosine similarity

--- src/CosineSimilarity.java
+++ src/CosineSimilarity.java
@@ -1,4 +1,20 @@
+import javafx.util.Pair;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+
+class SomeOtherSort implements Comparator<Pair<Email, Double>> {
+
+    public int compare(Pair<Email, Double> a, Pair<Email, Double> b) {
+        if (a.getValue() > b.getValue()) {
+            return -1;
+        }
+        return 1;
+    }
+
+}
 
 public class CosineSimilarity {
 
@@ -6,9 +22,16 @@ public class CosineSimilarity {
     ArrayList<Word> words;
     Email email1;
     Email email2;
-    ArrayList<Email> neighbours = new ArrayList<>();
+    ArrayList<Pair<Email, Double>> neighbours = new ArrayList<>();
     int k = 0;
     double minSimilarityScore = Double.MAX_VALUE;
+    double[] calArray;
+
+
+    public CosineSimilarity(int numberOfNeighbors) {
+        k = numberOfNeighbors;
+        calArray = new double[k];
+    }
 
 
     public double cosineSimilarity(Email e1, Email e2) {
@@ -31,9 +54,17 @@ public class CosineSimilarity {
 
     public void addNeighbour(Email email, double similarityScore) {
         if (similarityScore < minSimilarityScore) {
-            for (Email e : neighbours) {
 
+            if (neighbours.size() < k) {
+                neighbours.add(new Pair<Email, Double>(email, similarityScore));
+                minSimilarityScore = similarityScore;
+                return;
             }
+
+            Collections.sort(neighbours, new SomeOtherSort());
+            neighbours.remove(0);
+            neighbours.add(new Pair<Email, Double>(email, similarityScore));
+
         }
 
     }
@@ -44,7 +75,7 @@ public class CosineSimilarity {
 
         for (Email email : emails) {
             double similarityScore = cosineSimilarity(email, unclassified);
-
+            addNeighbour(email, similarityScore);
         }
 
         return emails;
