06b44988ae39af8b55e222efbf6e3486af509ac0
cliffton <cliffton.fernandes.rulz@gmail.com>
2018-11-05 00:59:57 -0500

    seq prog changed

--- src/EmailClassifierSeq.java
+++ src/EmailClassifierSeq.java
@@ -1,14 +1,13 @@
 import edu.rit.pj2.Task;
-import javafx.util.Pair;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class EmailClassifierSeq extends Task {
 
 
     ArrayList<Word> words;
-    ArrayList<Email> emails;
+    ArrayList<Email> unClassifiedEmails;
+    ArrayList<Email> classifiedEmails;
 
 
     /**
@@ -28,21 +27,22 @@ public class EmailClassifierSeq extends Task {
 
             MakeScores ms = new MakeScores();
             words = ms.getWords();
-            emails = ms.getEmails();
+            classifiedEmails = ms.getClassifiedEmails();
             NeighbourVbl neighbourVbl = new NeighbourVbl(k);
 
 
-            Email unclassified = new Email("I want to sell toys", 2);
+            unClassifiedEmails = ms.getUnClassifiedEmails();
 
-            for (Email email : emails) {
-                double similarityScore = neighbourVbl.cosineSimilarity(email, unclassified, words);
-                neighbourVbl.addNeighbour(similarityScore, email);
+            for (Email unclassified : unClassifiedEmails) {
 
+                for (Email email : classifiedEmails) {
+                    double similarityScore = neighbourVbl.cosineSimilarity(email, unclassified, words);
+                    neighbourVbl.addNeighbour(similarityScore, email);
+
+                }
+                int category = neighbourVbl.voting();
+                System.out.println(category);
             }
-
-            int category = neighbourVbl.voting();
-
-            System.out.println(category);
 
 
         } catch (Exception e) {
