75af4b3bdaadcd186ad557dfb9f8acf9ef606437
cliffton <cliffton.fernandes.rulz@gmail.com>
2018-12-08 17:14:22 -0500

    pics

--- src/tupleToSortWords.java
+++ src/tupleToSortWords.java
@@ -1,19 +1,19 @@
-public class tupleToSortWords implements Comparable{
+public class tupleToSortWords implements Comparable {
     String word;
     double tfidfScore;
 
-    tupleToSortWords(String word, double tfidfScore){
+    tupleToSortWords(String word, double tfidfScore) {
         this.word = word;
         this.tfidfScore = tfidfScore;
     }
 
     @Override
     public int compareTo(Object o) {
-        tupleToSortWords temp = (tupleToSortWords)o;
+        tupleToSortWords temp = (tupleToSortWords) o;
         return Double.compare(temp.getTfidfScore(), this.getTfidfScore());
     }
 
-    public double getTfidfScore(){
+    public double getTfidfScore() {
         return tfidfScore;
     }
 
